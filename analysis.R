a2347ed84377e676717d4f0a59c8c78d66194043
Asrithareddy13 <mdh3998@wmich.edu>
2024-01-14 15:30:56 -0500

    added comments, variables, functions

---
+++ analysis.R
@@ -0,0 +1,18 @@
+#This is an example of R 
+
+# Creating variables
+x <- 11
+y = 6
+
+# Variable names 
+my_variable <- "Hello, R!" 
+
+# Defining a function
+add_numbers <- function(a, b) {
+  result <- a + b
+  return(result)
+}
+
+# Calling the function
+sum_result <- add_numbers(3, 4)
+print(sum_result)
